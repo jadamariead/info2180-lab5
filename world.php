0b7bd185f2121eda15650ad457a87425ad15c90c
jadamariead <jadamariead@gmail.com>
2024-12-18 23:40:04 -0500

    Exercise 4

--- world.php
+++ world.php
@@ -21,9 +21,29 @@ if (isset($_GET['country'])) {
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-header('Content-Type: application/json');
-echo json_encode($results);
+if ($results){
+     echo '<thead>';
+     echo '<tr>';
+     echo '<th>Country Name</th>';
+     echo '<th>Continent</th>';
+     echo '<th>Independence Year</th>';
+     echo '<th>Head of State</th>';
+     echo '</tr>';
+     echo '</thead>';
+     echo '<tbody>';
+     foreach ($results as $row) {
+          echo '<tr>';
+          echo '<td>' . htmlspecialchars($row['name']) . '</td>';
+          echo '<td>' . htmlspecialchars($row['continent']) . '</td>';
+          echo '<td>' . htmlspecialchars($row['independence_year']) . '</td>';
+          echo '<td>' . htmlspecialchars($row['head_of_state']) . '</td>';
+          echo '</tr>';
+     }
+     echo '</tbody>';
+     echo '</table>';
+} else {
+     echo "Country not found";
+}
 
 
 ?>
